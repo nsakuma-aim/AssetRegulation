c61b3827b8f665af7ef25fd771d728158ca48efb
nsakuma-aim <nsakuma@aiming-inc.com>
2024-09-02 19:26:08 +0900

    create Preset and Add PresetManager

--- Assets/Scripts/Editor/AddRegulation.cs
+++ Assets/Scripts/Editor/AddRegulation.cs
@@ -1,9 +1,12 @@
+using System.IO;
 using System.Linq;
 using AssetRegulationManager.Editor.Core.Data;
 using AssetRegulationManager.Editor.Core.Model.AssetRegulations;
 using AssetRegulationManager.Editor.Core.Model.AssetRegulations.AssetConstraintImpl;
 using AssetRegulationManager.Editor.Core.Model.AssetRegulations.AssetFilterImpl;
 using UnityEditor;
+using UnityEditor.Presets;
+using UnityEngine;
 
 public static class AddRegulation
 {
@@ -12,7 +15,28 @@ public static class AddRegulation
     private static void AddSelectObjRegulation()
     {
         var obj = Selection.activeObject;
+        var texture = obj as Texture;
+        var path = AssetDatabase.GetAssetPath(texture);
+        var importer = AssetImporter.GetAtPath(path) as TextureImporter;
 
+        var preset = new Preset(importer);
+        var presetPath = Path.GetDirectoryName(path) + "/" + texture + "_Preset.preset";
+        AssetDatabase.CreateAsset(preset, presetPath);
+
+        var presetType = preset.GetPresetType();
+
+        if (presetType.IsValid())
+        {
+            var tmp = Preset.GetDefaultPresetsForType(presetType).ToList();
+            tmp.Add(new DefaultPreset("", preset));
+            Preset.SetDefaultPresetsForType(presetType, tmp.ToArray());
+        }
+
+        AddAssetRegulation();
+    }
+
+    private static void AddAssetRegulation()
+    {
         var regulationGUID = AssetDatabase.FindAssets("t:AssetRegulationSetStore").Single();
 
         var assetRegulationSetStore = AssetDatabase.LoadAssetAtPath<AssetRegulationSetStore>(AssetDatabase.GUIDToAssetPath(regulationGUID));
